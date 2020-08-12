b633dbe4f2af475b7185debb7d8362830ce80e7a
冯天如 <fengtr@dustess.com>
2020-08-12 18:06:23 +0800

    feat: 🎸 格式化代码

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { url } from 'inspector';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,13 +14,16 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-generate-code.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	// let disposable = vscode.commands.registerCommand('vscode-generate-code.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-generate-code!');
-	});
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World from vscode-generate-code!');
+	// });
+	context.subscriptions.push(vscode.commands.registerCommand('extension.demo.getCurrentFilePath', (uri) => {
+		vscode.window.showInformationMessage(`当前文件(夹)路径是：${uri ? uri.path : '空'}`);
+	}));
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
